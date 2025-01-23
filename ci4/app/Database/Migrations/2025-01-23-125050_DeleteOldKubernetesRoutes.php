1853c9e6c04df3904bc7e46d595cea93688d07c7
martin <martin@4spaces.dk>
2025-01-23 13:52:41 +0100

    DeleteOldKubernetesRoutes

---
+++ ci4/app/Database/Migrations/2025-01-23-125050_DeleteOldKubernetesRoutes.php
@@ -0,0 +1,27 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use RestExtension\Entities\ApiRoute;
+use RestExtension\Models\ApiRouteModel;
+
+class DeleteOldKubernetesRoutes extends Migration {
+
+    public function up() {
+        /** @var ApiRoute $apiRoutes */
+        $apiRoutes = (new ApiRouteModel())
+            ->whereIn('from', [
+                'kubernetes/namespaces/(.*)/pods/(.*)/exec',
+                'kubernetes/namespaces/(.*)/pods/(.*)/logs/watch',
+                'kubernetes/namespaces/(.*)/pods/(.*)/logs',
+            ])
+            ->find();
+        if ($apiRoutes->exists()) {
+            $apiRoutes->delete();
+        }
+    }
+
+    public function down() {
+
+    }
+
+}
